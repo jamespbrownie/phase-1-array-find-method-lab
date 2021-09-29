c9889987e6a3906e3c35a672c75b0db2a5b6e621
James Brownie <jamespbrownie@gmail.com>
2021-09-28 20:45:07 -0500

    commit tree 48106a6c91fa87f7a417354a268f672d1bbcb1ed

--- index.js
+++ index.js
@@ -1,1 +1,97 @@
 
+
+const winFinder = obj => {
+    return obj.result === "W"
+}
+
+const superbowlWin = array => {
+    let newArray = array.find(winFinder)
+    if (newArray) {
+    return newArray.year
+    } else {
+        return undefined
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const findWin = (element) => { return element.result === "W" }
+
+// const superbowlWin = (record) => {
+//     let win = record.find(findWin)
+//     if (win) {
+//         return win.year 
+//     } else {
+//         return undefined
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function superbowlWin(array) {
+//     let win = array.find( 
+//         function (par) {
+//             return (par.result === "W")
+//         }
+//     )
+//     if (win) {
+//         return win.year
+//     }
+//     else {
+//         return undefined
+//     }
+// }
+
